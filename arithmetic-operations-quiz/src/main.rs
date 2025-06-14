a89e722ba9c3c0d1e47358f9c6f6e886b3c0bdc4
hrshishym <chappy.dogheart@gmail.com>
2025-06-15 01:01:50 +0900

    クイズ： rand::rng() 利用

--- arithmetic-operations-quiz/src/main.rs
+++ arithmetic-operations-quiz/src/main.rs
@@ -1,5 +1,9 @@
+use rand::Rng;
+
 fn main() {
-    println!("1 + 1 = ??");
+    let op1 = rand::rng().random_range(1..=100);
+    let op2 = rand::rng().random_range(1..=100);
+    println!("{} + {} = ??", op1, op2);
     println!("?? の値を入力してください：");
 
     let mut ans_input = String::new();
@@ -11,26 +15,24 @@ fn main() {
     let ans_input = ans_input.trim().parse::<u32>().unwrap();
     // dbg!(ans_input);
 
-    if ans_input == 1 + 1 {
+    if ans_input == op1 + op2 {
         println!("正解です！");
     } else {
         println!("不正解です。");
     }
 
-    println!("1 - 4 = ??");
+    let op1 = rand::rng().random_range(1..=100);
+    let op2 = rand::rng().random_range(1..=100);
+    println!("{} - {} = ??", op1, op2);
     println!("?? の値を入力してください：");
     
     let mut ans_input = String::new();
     std::io::stdin().read_line(&mut ans_input).unwrap();
     let ans_input = ans_input.trim().parse::<i32>().unwrap();
-    if ans_input == 1 - 4 {
+    if ans_input == op1 - op2 {
         println!("正解です！");
     } else {
         println!("不正解です。");
     }
 
-    println!("i32 が扱えるデータ範囲： {} - {}", i32::MIN, i32::MAX);
-    println!("u32 が扱えるデータ範囲： {} - {}", u32::MIN, u32::MAX);
-    println!("i64 が扱えるデータ範囲： {} - {}", i64::MIN, i64::MAX);
-    println!("u64 が扱えるデータ範囲： {} - {}", u64::MIN, u64::MAX);
 }
