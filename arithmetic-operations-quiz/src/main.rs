d860ed7f20405d0c5d7f394689eeea9432bc1082
hrshishym <chappy.dogheart@gmail.com>
2025-06-19 00:26:53 +0900

    クイズ：正解数3でbreak

--- arithmetic-operations-quiz/src/main.rs
+++ arithmetic-operations-quiz/src/main.rs
@@ -1,38 +1,49 @@
 use rand::Rng;
 
 fn main() {
-    let op1 = rand::rng().random_range(1..=100);
-    let op2 = rand::rng().random_range(1..=100);
-    println!("{} + {} = ??", op1, op2);
-    println!("?? の値を入力してください：");
+    let mut num_of_correct = 0;
 
-    let mut ans_input = String::new();
+    while num_of_correct < 3 {
+        
+        let op1 = rand::rng().random_range(1..=100);
+        let op2 = rand::rng().random_range(1..=100);
+        println!("{} + {} = ??", op1, op2);
+        println!("?? の値を入力してください：");
 
-    std::io::stdin().read_line(&mut ans_input).unwrap();
+        let mut ans_input = String::new();
 
-    // dbg!(&ans_input);
+        std::io::stdin().read_line(&mut ans_input).unwrap();
 
-    let ans_input = ans_input.trim().parse::<u32>().unwrap();
-    // dbg!(ans_input);
+        // dbg!(&ans_input);
 
-    if ans_input == op1 + op2 {
-        println!("正解です！");
-    } else {
-        println!("不正解です。");
+        let ans_input = ans_input.trim().parse::<u32>().unwrap();
+        // dbg!(ans_input);
+
+        if ans_input == op1 + op2 {
+            println!("正解です！");
+            num_of_correct += 1;
+            if num_of_correct >= 3 {
+                break;
+            }
+        } else {
+            println!("不正解です。");
+        }
+
+        let op1 = rand::rng().random_range(1..=100);
+        let op2 = rand::rng().random_range(1..=100);
+        println!("{} - {} = ??", op1, op2);
+        println!("?? の値を入力してください：");
+        
+        let mut ans_input = String::new();
+        std::io::stdin().read_line(&mut ans_input).unwrap();
+        let ans_input = ans_input.trim().parse::<i32>().unwrap();
+        if ans_input == op1 - op2 {
+            println!("正解です！");
+            num_of_correct += 1;
+        } else {
+            println!("不正解です。");
+        }
     }
 
-    let op1 = rand::rng().random_range(1..=100);
-    let op2 = rand::rng().random_range(1..=100);
-    println!("{} - {} = ??", op1, op2);
-    println!("?? の値を入力してください：");
-    
-    let mut ans_input = String::new();
-    std::io::stdin().read_line(&mut ans_input).unwrap();
-    let ans_input = ans_input.trim().parse::<i32>().unwrap();
-    if ans_input == op1 - op2 {
-        println!("正解です！");
-    } else {
-        println!("不正解です。");
-    }
-
+    println!("3問正解しました！おめでとうございます！");
 }
